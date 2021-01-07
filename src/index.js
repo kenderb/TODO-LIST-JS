280efca1ffe3f5627a9a903e352cb4668231eff9
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-07 11:13:44 -0500

    Add base colors for the app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './reset.scss';
+import './colors.scss';
 import Todo from './components/todo/todo.component';
+
 console.log('Hello Todo app this is fun');
 const todo = new Todo('kender');
 todo.sayHello();
