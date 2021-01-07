60d057ae9cbbae70f41076c159011350227cca7b
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-07 11:19:10 -0500

    Add a general style file for the app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import './reset.scss';
-import './colors.scss';
+import './styles/reset.scss';
+import './styles/colors.scss';
+import './styles/general.scss';
 import Todo from './components/todo/todo.component';
 
 console.log('Hello Todo app this is fun');
