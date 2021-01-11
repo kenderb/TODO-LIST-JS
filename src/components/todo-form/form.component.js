8a4ef80bdd3fadb134ce1877d38b172da107470b
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-11 16:52:30 -0500

    Add some events for the form

--- src/components/todo-form/form.component.js
+++ src/components/todo-form/form.component.js
@@ -4,7 +4,7 @@ import Project from '../project/project.component';
 export default class Form {
   constructor(container, btn) {
     this.container = container;
-    this.form = this.container.children[0]
+    this.form = this.container.children[1]
     this.btn = btn;
   }
 
@@ -19,27 +19,10 @@ export default class Form {
     if (!data.title || !data.date || !data.description) {
       return Form.renderErrorValidation();
     }
-    console.log(data);
     return true;
   }
 
-  static saveData(form){
-    const project = form.elements.project.value;
-    const title = form.elements.title.value;
-    const date = form.elements.date.value;
-    const description = form.elements.description.value;
-    const priority = form.elements.priority.value;
-    
-    const todo = new Todo(
-      project,
-      title,
-      date,
-      description,
-      priority
-    );
-    
-    Form.formValidation(todo);
-  }
+ 
 
   static validateProjectForm(title){
     if (!title) {
@@ -60,31 +43,72 @@ export default class Form {
 
   static getProjectTitleAndColor(projectForm){
     const errorContainer = document.getElementById('errors');
-    errorContainer.innerHTML = '';
     const title = projectForm.elements.projectTitle.value;
+    errorContainer.innerHTML = '';
     if (Form.validateProjectForm(title)) return false;
     const color = projectForm.elements.color.value;
-    if (!Form.validateProjectUniquenes(title, errorContainer))  return false;
-    console.log('Add new project to the form named: ' + title);
+    if (!Form.validateProjectUniquenes(title, errorContainer)) return false;
     const project =  new Project(title, color);
+    return project;
+  }
+  static closeForm(container) {
+    container.classList.toggle('d-none');
+  }
+  
+  static addNewProjectToForm(project){
+    if (!project) return false;
+    const projectFormContainer = document.getElementById('project-form-container');
+    const projectListselect = document.getElementById('project-list');
+    const option = document.createElement('option');
+    option.value = project.color;
+    option.id = project.title;
+    option.innerHTML = project.title;
+    projectListselect.append(option);
+    Form.closeForm(projectFormContainer);
     return project;
   }
 
   static getProjectFormInfo() {
     const projectForm = document.getElementById('project-form');
+    const projectFormContainer = document.getElementById('project-form-container');
     projectForm.addEventListener('click', (e) => {
       if (e.target.id === 'btn-create-project') {
         const project = Form.getProjectTitleAndColor(projectForm);
-        return project;
+        return Form.addNewProjectToForm(project);
+      }
+
+      if (e.target.id === 'cancel-project-button') {
+        Form.closeForm(projectFormContainer);
+        return false;
       }
     });
   }
+  
+  static saveData(form){
+    const project = form.elements.project.value
+    const title = form.elements.title.value;
+    const date = form.elements.date.value;
+    const description = form.elements.description.value;
+    const priority = form.elements.priority.value;
+    console.dir(form.elements.project);
+    
+    const todo = new Todo(
+      project,
+      title,
+      date,
+      description,
+      priority
+    );
+    
+    Form.formValidation(todo);
+    return todo;
+  }
 
   getFromData() {
-    const project = Form.getProjectFormInfo();
+    Form.getProjectFormInfo();
     this.form.addEventListener("submit", (e) => {
       e.preventDefault();
-      Form.saveData(this.form);
+      console.log(Form.saveData(this.form));
     });
   }
   
