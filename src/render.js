04a1266c2bafb3b0d52491f4eb899f716d6396a4
kenderb <40863532+kenderb@users.noreply.github.com>
2021-01-12 22:19:37 -0500

    Refactor the to-do card render

--- src/render.js
+++ src/render.js
@@ -39,6 +39,105 @@ export default class Render {
     }
   }
   
+  static eventForClick(todo, todoDiv, project, data){
+    const deleteTitleSpaces = (todo.title).replace(/\s/g, '');
+    todoDiv.addEventListener('click', (e) => {
+      if (e.target.id === `title-${deleteTitleSpaces}-${project.name}` || 
+          e.target.id === `date-${deleteTitleSpaces}-${project.name}`  ||
+          e.target.id === `container-${deleteTitleSpaces}-${project.name}`) {
+        const detailsContainer = document.getElementById(`details-${deleteTitleSpaces}-${project.name}`)
+        detailsContainer.classList.toggle('d-none');
+      }
+      if (e.target.id === `delete-${deleteTitleSpaces}-${project.name}`) {
+        const currentTodo = document.getElementById(`container-${deleteTitleSpaces}-${project.name}`);
+        const currentDetail = document.getElementById(`details-${deleteTitleSpaces}-${project.name}`);
+        const indexOfItem = (project.todos).indexOf(todo);
+        if (indexOfItem > -1) {
+          (project.todos).splice(indexOfItem, 1);
+          currentTodo.remove();
+          currentDetail.remove();
+          Render.saveDataToTheLocalStorage('todoApp', data);
+        }
+      }
+    });
+  }
+
+  static eventForFocusOut(todoDiv, project, todo, data){
+    const deleteTitleSpaces = (todo.title).replace(/\s/g, '');
+    todoDiv.addEventListener('focusout', (e) => {
+      console.log(e.target.id);
+      if (e.target.id === `description-${deleteTitleSpaces}-${project.name}`) {
+        todo.description = e.target.innerHTML;
+        Render.saveDataToTheLocalStorage('todoApp', data);
+      }
+
+      if (e.target.id === `editable-title-${deleteTitleSpaces}-${project.name}`) {
+        todo.title = e.target.innerHTML;
+        Render.saveDataToTheLocalStorage('todoApp', data);
+        location.reload();
+      }
+
+      if (e.target.id === `priority-${deleteTitleSpaces}-${project.name}`) {
+        console.log(e.target.value);
+        todo.priority = e.target.value;
+        Render.saveDataToTheLocalStorage('todoApp', data);
+        location.reload();
+      }
+      
+      if (e.target.id === `edit-date-${deleteTitleSpaces}-${project.name}`) {
+        todo.date = e.target.value;
+        Render.saveDataToTheLocalStorage('todoApp', data);
+        location.reload();
+      }
+    });
+  }
+
+  static renderTodoCard(deleteTitleSpaces, project, todo){
+    const todoCard =  `
+      <div class=" todo d-flex" id="container-${deleteTitleSpaces}-${project.name}">
+        <div class="checkbox-container"id="check-${deleteTitleSpaces}-${project.name}">
+          <input type="checkbox" name="checkbox" id="checkbox" class="checkbox" checked="checked">
+          <label for="checkbox" class="checkbox-circle" id="${deleteTitleSpaces}-${project.name}"></label>
+        </div>
+        <p class="todo-title" id="title-${deleteTitleSpaces}-${project.name}">
+          ${todo.title}
+        </p>
+        <div class="todo-date ml-auto gray-color" id="date-${deleteTitleSpaces}-${project.name}"> Due date: ${todo.date}</div>
+        <ion-icon name="trash-outline" class="delete-todo-icon" id="delete-${deleteTitleSpaces}-${project.name}"></ion-icon>
+        <div class="priority-color-container priority-color-${todo.priority}"></div>
+      </div>
+      <div class="todo-details d-none" id="details-${deleteTitleSpaces}-${project.name}">
+        <h4 contenteditable="true" id="editable-title-${deleteTitleSpaces}-${project.name}">${todo.title}</h4>
+        <p>Due date:</p><input type="date" value="${todo.date}" id="edit-date-${deleteTitleSpaces}-${project.name}">
+        <p>Description: </p>
+        <p class="todo-description" id="description-${deleteTitleSpaces}-${project.name}">
+            ${todo.description}
+        </p> <br/>
+        <p>Priority: </p>
+        <select id="priority-${deleteTitleSpaces}-${project.name}">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+      </div>
+    `;
+    return todoCard;
+  }
+
+  static construcCard(todoContainer, project, todo, data){
+    const todoDiv = document.createElement('div');
+    const deleteTitleSpaces = (todo.title).replace(/\s/g, '');
+    todoDiv.id = `todo-${deleteTitleSpaces}`;
+    todoDiv.innerHTML = Render.renderTodoCard(deleteTitleSpaces, project, todo);
+    Render.eventForClick(todo, todoDiv, project, data);
+    Render.eventForFocusOut(todoDiv, project, todo, data);
+    todoContainer.append(todoDiv);
+    const getCircle = document.getElementById(`${deleteTitleSpaces}-${project.name}`);
+    const editableDescription = document.getElementById(`description-${deleteTitleSpaces}-${project.name}`);
+    editableDescription.contentEditable = true;
+    getCircle.style.border = `${project.color} 1px solid`;
+  }
+
   renderAllTodos() {
     const data = Render.getDatafromTheLocalStorage('todoApp');
     const todoContainer = document.getElementById('all-todos-container');
@@ -46,88 +145,7 @@ export default class Render {
     if (data) {
       for (const project of data) {
         for (const todo of project.todos) {
-          const todoDiv = document.createElement('div');
-          const deleteTitleSpaces = (todo.title).replace(/\s/g, '');
-          todoDiv.id = `todo-${deleteTitleSpaces}`;
-          todoDiv.innerHTML = `
-            <div class=" todo d-flex" id="container-${deleteTitleSpaces}-${project.name}">
-              <div class="checkbox-container"id="check-${deleteTitleSpaces}-${project.name}">
-                <input type="checkbox" name="checkbox" id="checkbox" class="checkbox" checked="checked">
-                <label for="checkbox" class="checkbox-circle" id="${deleteTitleSpaces}-${project.name}"></label>
-              </div>
-              <p class="todo-title" id="title-${deleteTitleSpaces}-${project.name}">
-                ${todo.title}
-              </p>
-              <div class="todo-date ml-auto gray-color" id="date-${deleteTitleSpaces}-${project.name}"> Due date: ${todo.date}</div>
-              <ion-icon name="trash-outline" class="delete-todo-icon" id="delete-${deleteTitleSpaces}-${project.name}"></ion-icon>
-              <div class="priority-color-container priority-color-${todo.priority}"></div>
-            </div>
-            <div class="todo-details d-none" id="details-${deleteTitleSpaces}-${project.name}">
-              <h4 contenteditable="true" id="editable-title-${deleteTitleSpaces}-${project.name}">${todo.title}</h4>
-              <p>Due date:</p><input type="date" value="${todo.date}" id="edit-date-${deleteTitleSpaces}-${project.name}">
-              <p>Description: </p>
-              <p class="todo-description" id="description-${deleteTitleSpaces}-${project.name}">
-                  ${todo.description}
-              </p> <br/>
-              <p>Priority: </p>
-              <select id="priority-${deleteTitleSpaces}-${project.name}">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-              </select>
-            </div>
-          `;
-          todoDiv.addEventListener('click', (e) => {
-            if (e.target.id === `title-${deleteTitleSpaces}-${project.name}` || 
-                e.target.id === `date-${deleteTitleSpaces}-${project.name}`  ||
-                e.target.id === `container-${deleteTitleSpaces}-${project.name}`) {
-              const detailsContainer = document.getElementById(`details-${deleteTitleSpaces}-${project.name}`)
-              detailsContainer.classList.toggle('d-none');
-            }
-            if (e.target.id === `delete-${deleteTitleSpaces}-${project.name}`) {
-              const currentTodo = document.getElementById(`container-${deleteTitleSpaces}-${project.name}`);
-              const currentDetail = document.getElementById(`details-${deleteTitleSpaces}-${project.name}`);
-              const indexOfItem = (project.todos).indexOf(todo);
-              if (indexOfItem > -1) {
-                (project.todos).splice(indexOfItem, 1);
-                currentTodo.remove();
-                currentDetail.remove();
-                Render.saveDataToTheLocalStorage('todoApp', data);
-              }
-            }
-          });
-          todoDiv.addEventListener('focusout', (e) => {
-            console.log(e.target.id);
-            if (e.target.id === `description-${deleteTitleSpaces}-${project.name}`) {
-              todo.description = e.target.innerHTML;
-              Render.saveDataToTheLocalStorage('todoApp', data);
-            }
-
-            if (e.target.id === `editable-title-${deleteTitleSpaces}-${project.name}`) {
-              todo.title = e.target.innerHTML;
-              Render.saveDataToTheLocalStorage('todoApp', data);
-              location.reload();
-            }
-
-            if (e.target.id === `priority-${deleteTitleSpaces}-${project.name}`) {
-              console.log(e.target.value);
-              todo.priority = e.target.value;
-              Render.saveDataToTheLocalStorage('todoApp', data);
-              location.reload();
-            }
-            
-            if (e.target.id === `edit-date-${deleteTitleSpaces}-${project.name}`) {
-              todo.date = e.target.value;
-              Render.saveDataToTheLocalStorage('todoApp', data);
-              location.reload();
-            }
-          });
-
-          todoContainer.append(todoDiv);
-          const getCircle = document.getElementById(`${deleteTitleSpaces}-${project.name}`);
-          const editableDescription = document.getElementById(`description-${deleteTitleSpaces}-${project.name}`);
-          editableDescription.contentEditable = true;
-          getCircle.style.border = `${project.color} 1px solid`;
+          Render.construcCard(todoContainer, project, todo, data);
         }
       }
     }
